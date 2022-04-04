c67c516c1249165d2756f2c801969f97f2652b84
nima.bidarian <bidarian.n@gmail.com>
2022-04-04 12:09:13 +0430

    task_user migration fix

--- database/migrations/2022_03_31_071437_create_task_user_table.php
+++ database/migrations/2022_03_31_071437_create_task_user_table.php
@@ -32,7 +32,11 @@ return new class extends Migration
     public function down()
     {
         Schema::create('task_user', function (Blueprint $table) {
-            //
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('task_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');;
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');;
         });
     }
 };
